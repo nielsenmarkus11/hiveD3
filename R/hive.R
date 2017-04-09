47ce156f4d29fce3b99cba45143286ad53029ef0
Mark Nielsen <nielsen.markus11@gmail.com>
2017-04-08 23:23:05 -0600

    Finish documentation of the package.

--- R/hive.R
+++ R/hive.R
@@ -1,6 +1,40 @@
-#' <Add Title>
+#' Create a D3 JavaScript hive network plot
 #'
-#' <Add Description>
+#' This package in an implementation of the hive network graph developed by Martin Krzysinski (see
+#' \url{http://egweb.bcgsc.ca/}) and implemented in D3 by Mike Bostock and Rich Morin
+#' (see \url{https://bost.ocks.org/mike/hive/} and
+#' \url{https://bl.ocks.org/mbostock/2066415}).
+#'
+#' @param links a data frame with the link definitions between the nodes. Both
+#' the \code{source} node id and \code{target} node id should be included for each
+#' link.
+#' @param nodes a data frame containing the node id and position. It should
+#' include the id, \code{x}, and position, \code{y}, for each node. The ids should be
+#' numbered starting from 0 and the positions between 0 and 1.
+#' @param innerRadius the spacing from the center of the graph to the inner radius of
+#' the hive plot in pixels.
+#' @param outerRadius the spacing from the center of the graph to the outer radius of
+#' the hive plot in pixels.
+#' @param width numeric width for the network graph's frame area in pixels.
+#' @param height numeric height for the network graph's frame area in pixels.
+#' @param elementId defined the HTML object's id.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' nodes = data.frame(x=c(0,0,1,1,2,2), y=c(.1,.9,.2,.3,.1,.8))
+#' links = data.frame(source=c(0,1,2,2,3,4,5),
+#'                    target=c(2,3,4,5,5,0,1))
+#'
+#' hive(nodes=nodes,links=links, width = "100%", height = "500px")
+#'
+#' }
+#'
+#' @source
+#' D3.js and d3.hive.js were created by Michael Bostock and the latter with
+#' the help of Rich Morin. Documentation can be found on \url{http://d3js.org/}
+#' and \url{https://bost.ocks.org/mike/hive/} respectively. Hive plots were
+#' developed by Martin Krzywinski, et al. \url{http://egweb.bcgsc.ca/}
 #'
 #' @import htmlwidgets
 #'
@@ -11,6 +45,7 @@ hive <- function(nodes, links, innerRadius=40, outerRadius = 240, width = NULL, 
   x = list(
     nodes = nodes,
     links = links,
+    numAxis = max(nodes$x)+1,
     options = list(innerRadius=innerRadius,
                    outerRadius=outerRadius)
   )
--- inst/htmlwidgets/hive.js
+++ inst/htmlwidgets/hive.js
@@ -10,58 +10,39 @@ HTMLWidgets.widget({
 
     return {
 
-      resize: function(width, height) {
-
-        // TODO: code to re-render the widget with a new size
-        d3.select(el).append("svg")
-          .attr("width", width)
-          .attr("height", height)
-          .append("g")
-          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-      },
-
-      renderValue: function() {
+      renderValue: function(x) {
 
         // alias options
-        //var options = x.options;
+        var options = x.options;
 
         // convert links and nodes data frames to d3 friendly format
-        //var links = HTMLWidgets.dataframeToD3(x.links);
-        //var nodes = HTMLWidgets.dataframeToD3(x.nodes);
-        var nodes = [
-          {x: 0, y: .1},
-          {x: 0, y: .9},
-          {x: 1, y: .2},
-          {x: 1, y: .3},
-          {x: 2, y: .1},
-          {x: 2, y: .8}
-        ];
+        var nodes = HTMLWidgets.dataframeToD3(x.nodes);
+        var prelinks = HTMLWidgets.dataframeToD3(x.links);
 
-        var links = [
-          {source: nodes[0], target: nodes[2]},
-          {source: nodes[1], target: nodes[3]},
-          {source: nodes[2], target: nodes[4]},
-          {source: nodes[2], target: nodes[5]},
-          {source: nodes[3], target: nodes[5]},
-          {source: nodes[4], target: nodes[0]},
-          {source: nodes[5], target: nodes[1]}
-        ];
+        var links = [];
+        prelinks.forEach(function(d){
+          var tmp = {};
+          tmp["source"]=nodes[d.source];
+          tmp["target"]=nodes[d.target];
+          links.push(tmp);
+        })
 
-        //var innerRadius = options.innerRadius,
-        //    outerRadius = options.outerRadius;
-        var innerRadius = 40,
-            outerRadius = 240;
+        var innerRadius = options.innerRadius,
+            outerRadius = options.outerRadius;
 
-        var angle = d3.scale.ordinal().domain(d3.range(4)).rangePoints([0, 2 * Math.PI]),
+        var angle = d3.scale.ordinal().domain(d3.range(x.numAxis+1)).rangePoints([0, 2 * Math.PI]),
             radius = d3.scale.linear().range([innerRadius, outerRadius]),
             color = d3.scale.category10().domain(d3.range(20));
 
         // select the svg element and remove existing children
-        var svg = d3.select(el).select("svg");
-        svg.selectAll("*").remove();
+        var svg = d3.select(el).append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
         svg.selectAll(".axis")
-            .data(d3.range(3))
+            .data(d3.range(x.numAxis))
           .enter().append("line")
             .attr("class", "axis")
             .attr("transform", function(d) { return "rotate(" + degrees(angle(d)) + ")"; })
