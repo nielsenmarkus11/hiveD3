9df351533dc1f69dbefae0b83a88e34ca8216434
Mark Nielsen <nielsen.markus11@gmail.com>
2017-06-02 18:01:57 -0600

    Add functions for the presentation

---
+++ R/hive_no_int.R
@@ -0,0 +1,103 @@
+#' Create a D3 JavaScript hive network plot
+#'
+#' This package in an implementation of the hive network graph developed by Martin Krzysinski (see
+#' \url{http://egweb.bcgsc.ca/}) and implemented in D3 by Mike Bostock and Rich Morin
+#' (see \url{https://bost.ocks.org/mike/hive/} and
+#' \url{https://bl.ocks.org/mbostock/2066415}).
+#'
+#' @param links a data frame with the link definitions between the nodes. Both
+#' the \code{source} node id and \code{target} node id should be included for each
+#' link.
+#' @param nodes a data frame containing the node id and position. It should
+#' include the id, \code{id}, the axis id, \code{x}, and position, \code{y}, for each node. Both ids should be
+#' numbered starting from 0 and the positions between 0 and 1.
+#' @param innerRadius the spacing from the center of the graph to the inner radius of
+#' the hive plot in pixels.
+#' @param outerRadius the spacing from the center of the graph to the outer radius of
+#' the hive plot in pixels.
+#' @param opacity the opacity of the links.
+#' @param width numeric width for the network graph's frame area in pixels.
+#' @param height numeric height for the network graph's frame area in pixels.
+#' @param elementId defined the HTML object's id.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' nodes = data.frame(id=c(0,1,2,3,4,5),x=c(0,0,1,1,2,2), y=c(.1,.9,.2,.3,.1,.8))
+#' links = data.frame(source=c(0,1,2,2,3,4,5),
+#'                    target=c(2,3,4,5,5,0,1))
+#'
+#' hive(nodes=nodes,links=links, width = "100%", height = "500px")
+#'
+#' }
+#'
+#' @source
+#' D3.js and d3.hive.js were created by Michael Bostock and the latter with
+#' the help of Rich Morin. Documentation can be found on \url{http://d3js.org/}
+#' and \url{https://bost.ocks.org/mike/hive/} respectively. Hive plots were
+#' developed by Martin Krzywinski, et al. \url{http://egweb.bcgsc.ca/}
+#'
+#' @import htmlwidgets
+#'
+#' @export
+hive_no_int <- function(nodes, links, innerRadius=40, outerRadius = 240, opacity=0.7, width = NULL, height = NULL, elementId = NULL) {
+
+  # sort in order of node id
+  if("id" %in% colnames(nodes)) {
+    nodes <- nodes[order(nodes$id),]
+    nodes$id <- NULL
+  }
+
+  # color by axis if no coloring is supplied
+  if(!("color" %in% colnames(nodes))) {
+    nodes$color <- nodes$x
+  }
+
+  # forward options using x
+  x = list(
+    nodes = nodes,
+    links = links,
+    numAxis = max(nodes$x)+1,
+    options = list(innerRadius=innerRadius,
+                   outerRadius=outerRadius,
+                   opacity=opacity)
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'hive_no_int',
+    x,
+    width = width,
+    height = height,
+    package = 'hiveD3',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for hive
+#'
+#' Output and render functions for using hive within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a hive
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name hive-shiny
+#'
+#' @export
+hiveOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'hive_no_int', width, height, package = 'hiveD3')
+}
+
+#' @rdname hive-shiny
+#' @export
+renderHive <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, hiveOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/hive_no_int.js
@@ -0,0 +1,89 @@
+HTMLWidgets.widget({
+
+  name: 'hive_no_int',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // alias options
+        var options = x.options;
+
+        // convert links and nodes data frames to d3 friendly format
+        var nodes = HTMLWidgets.dataframeToD3(x.nodes);
+        var prelinks = HTMLWidgets.dataframeToD3(x.links);
+
+        // create json of link sources and targets
+        var links = [];
+        prelinks.forEach(function(d){
+          var tmp = {};
+          tmp.source=nodes[d.source];
+          tmp.target=nodes[d.target];
+          links.push(tmp);
+        });
+
+
+        var innerRadius = options.innerRadius,
+            outerRadius = options.outerRadius;
+
+        var angle = d3.scale.ordinal().domain(d3.range(x.numAxis+1)).rangePoints([0, 2 * Math.PI]),
+            radius = d3.scale.linear().range([innerRadius, outerRadius]),
+            color = d3.scale.category10().domain(d3.range(20));
+
+        // select the svg element and remove existing children
+        var svg = d3.select(el).append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g")
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        svg.selectAll(".axis")
+            .data(d3.range(x.numAxis))
+            .enter().append("line")
+            .attr("class", "axis")
+            .attr("transform", function(d) {
+              return "rotate(" + degrees(angle(d)) + ")";
+            })
+            .attr("x1", radius.range()[0])
+            .attr("x2", radius.range()[1]);
+
+        // draw links
+        var link = svg.selectAll(".link")
+            .data(links)
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", d3.hive.link()
+              .angle(function(d) { return angle(d.x); })
+              .radius(function(d) { return radius(d.y); }))
+            .style("stroke", function(d) { return color(d.source.color); })
+            .style("stroke-width", 1.5)
+            .style("opacity", options.opacity);
+
+        // draw nodes
+        var node = svg.selectAll(".node")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+              return "rotate(" + degrees(angle(d.x)) + ")";
+            })
+            .attr("cx", function(d) { return radius(d.y); })
+            .attr("r", 5)
+            .style("fill", function(d) { return color(d.color); })
+            .style("stroke", "#000000");
+
+        function degrees(radians) {
+          return radians / Math.PI * 180 - 90;
+        }
+
+      }
+
+    };
+  }
+});
